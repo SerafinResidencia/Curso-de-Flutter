4158e65872cda98cabd40716df9cfcfae5173ba7
serafin <serafin.martinez@devitm.com>
2025-10-16 13:26:23 -0600

    Completamos la apariencia base de la pantalla

--- session5/counter_app/lib/screens/home_screen.dart
+++ session5/counter_app/lib/screens/home_screen.dart
@@ -6,12 +6,34 @@ class HomeScreen extends StatelessWidget {
   //Al extenderse de un tipo de wigdet cambia.
   const HomeScreen({
     super.key,
-  }); //Sirve para identificaci+on dentro de un contexto.
+  }); //Sirve para identificación dentro de un contexto.
 
   @override
   Widget build(BuildContext context) {
-    return const /*Se agrga const 
-    por que ni este wigdet Padre ni 
-    sus hijos cambian de estado*/ Center(child: Text('Home Screen'));
+    TextStyle fontSize40 = const TextStyle(fontSize: 40);
+    //Build context - Permite construir la navegación
+    //y widgets que contenga.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomeScreen'),
+        elevation: 10,
+      ), //Crea una Barra en la superior de la pantalla.
+      backgroundColor: Color.fromARGB(255, 138, 159, 174),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Contador', style: fontSize40),
+            Text('0', style: fontSize40),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_circle_outline_sharp),
+        onPressed: () {
+          print('Hola');
+        },
+      ),
+    );
   }
-} // Esto es un widget
+}
