a78b3c54831ea9602775c2176c7393fa4a91eb6d
serafin <serafin.martinez@devitm.com>
2025-10-17 14:50:22 -0600

    68.- Mostrar List View 2

--- session6/fl_componentes/lib/main.dart
+++ session6/fl_componentes/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:fl_componentes/screens/listview1_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'package:fl_componentes/screens/listview2_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,6 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Material App', home: Listview1Screen());
+    return MaterialApp(title: 'Material App', home: Listview2Screen());
   }
 }
