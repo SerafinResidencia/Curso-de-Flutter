80190e7a4355773c1cef5f41995c26d1995e1699
serafin <serafin.martinez@devitm.com>
2025-10-17 13:33:13 -0600

    66.- Creación de Carpeta y archivo List view

---
+++ session6/fl_componentes/lib/screens/listview1_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+  const Listview1Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Listview Tipo 1')),
+
+      body: ListView(children: [Text('Hello World')]),
+    );
+  }
+}
