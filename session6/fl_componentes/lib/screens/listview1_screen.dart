147c717dc9d487ada23d2d561e4cf90172a4fd88
serafin <serafin.martinez@devitm.com>
2025-10-17 14:24:37 -0600

    67.- Creación de ListTile a partir de lista estática

--- session6/fl_componentes/lib/screens/listview1_screen.dart
+++ session6/fl_componentes/lib/screens/listview1_screen.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Listview1Screen extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'Doom Eternal',
+    'Super Smash',
+    'Borderlands',
+  ];
+
   const Listview1Screen({super.key});
 
   @override
@@ -8,7 +15,19 @@ class Listview1Screen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Listview Tipo 1')),
 
-      body: ListView(children: [Text('Hello World')]),
+      body: ListView(
+        children: [
+          ...options.map(
+            (game) => ListTile(
+              title: Text(game),
+              trailing: Icon(Icons.arrow_right_sharp),
+            ),
+          ),
+        ],
+
+        //Leading: su función es contener iconos
+        //children: [ListTile(leading: Icon(Icons.), title: Text('Hello World'))],
+      ),
     );
   }
 }
