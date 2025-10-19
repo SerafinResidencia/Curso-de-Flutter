2fb43c7017becf52841412ad313d7edd8f32c0f2
serafin <serafin.martinez@devitm.com>
2025-10-19 16:21:09 -0600

    74.- Agregamos la navegacion con la clase Menu Options

--- session6/fl_componentes/lib/router/app_router.dart
+++ session6/fl_componentes/lib/router/app_router.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
 
+import 'package:fl_componentes/models/models.dart';
 import 'package:fl_componentes/screens/screens.dart';
 
 class AppRoutes {
   static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    MenuOption(
+      route: 'home',
+      icon: Icons.home,
+      name: 'home',
+      screen: HomeScreen(),
+    ),
+    MenuOption(
+      route: 'listview1',
+      icon: Icons.list,
+      name: 'List View 1',
+      screen: Listview1Screen(),
+    ),
+    MenuOption(
+      route: 'listview2',
+      icon: Icons.list_alt_sharp,
+      name: 'List View 2',
+      screen: Listview2Screen(),
+    ),
+    MenuOption(
+      route: 'alert',
+      icon: Icons.campaign,
+      name: 'Alerts',
+      screen: AlertScreen(),
+    ),
+    MenuOption(
+      route: 'card',
+      icon: Icons.card_membership,
+      name: 'Cards',
+      screen: CardsScreen(),
+    ),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
     'listview1': (BuildContext context) => const Listview1Screen(),
     'listview2': (BuildContext context) => const Listview2Screen(),
     'home': (BuildContext context) => const HomeScreen(),
     'alert': (BuildContext context) => const AlertScreen(),
+    'card': (BuildContext context) => const CardsScreen(),
   };
 
   static Route<dynamic> onGenerateRoute(settings) {
