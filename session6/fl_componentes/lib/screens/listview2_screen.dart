c1e96b230da2d09fa695b603c89e6fc57f9019f5
serafin <serafin.martinez@devitm.com>
2025-10-19 14:31:26 -0600

    69.- Tap sobre los ListTiles y estilo a la ApBar

--- session6/fl_componentes/lib/screens/listview2_screen.dart
+++ session6/fl_componentes/lib/screens/listview2_screen.dart
@@ -13,13 +13,24 @@ class Listview2Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Listview Tipo 1')),
+      appBar: AppBar(
+        title: const Text('Listview Tipo 1'),
+        elevation: 0,
+        backgroundColor: const Color.fromARGB(255, 161, 27, 232),
+      ),
 
       body: ListView.separated(
         itemCount: options.length,
         itemBuilder: (BuildContext context, int index) => ListTile(
           title: Text(options[index]),
-          trailing: Icon(Icons.arrow_right_sharp),
+          trailing: Icon(
+            Icons.arrow_right_sharp,
+            color: Color.fromARGB(255, 161, 27, 232),
+          ),
+          onTap: () {
+            final game = options[index];
+            print(game);
+          },
         ),
         separatorBuilder: (_, __) => const Divider(),
         //Leading: su función es contener iconos
