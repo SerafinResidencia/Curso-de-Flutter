56a42ec99d7059e32728c2d2248fb838474029b4
serafin <serafin.martinez@devitm.com>
2025-10-19 14:55:47 -0600

    70.- COnfiguración de rutas de navegacación

--- session6/fl_componentes/lib/main.dart
+++ session6/fl_componentes/lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:fl_componentes/screens/home_screen.dart';
+import 'package:fl_componentes/screens/screens.dart';
 import 'package:flutter/material.dart';
-
-import 'package:fl_componentes/screens/listview2_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,6 +9,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Material App', home: Listview2Screen());
+    return MaterialApp(
+      title: 'Material App',
+      //home: Listview2Screen(),
+      initialRoute: 'home_screen',
+      routes: {
+        'listview1': (BuildContext context) => const Listview1Screen(),
+        'listview2': (BuildContext context) => const Listview2Screen(),
+        'home': (BuildContext context) => const HomeScreen(),
+      },
+    );
   }
 }
