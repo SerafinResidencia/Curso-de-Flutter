64aebb811c6316b12c50969173cefad3dcf094e2
serafin <serafin.martinez@devitm.com>
2025-10-19 15:29:41 -0600

    Agragamos la pantalla de alerta a las rutas de navegación

--- session6/fl_componentes/lib/screens/screens.dart
+++ session6/fl_componentes/lib/screens/screens.dart
@@ -1,2 +1,4 @@
+export 'package:fl_componentes/screens/alert_screen.dart';
+export 'package:fl_componentes/screens/home_screen.dart';
 export 'package:fl_componentes/screens/listview1_screen.dart';
 export 'package:fl_componentes/screens/listview2_screen.dart';
