829ebb8119686f69287ced5dd4394617f4c288eb
serafin <serafin.martinez@devitm.com>
2025-10-19 22:33:14 -0600

    79.- Creación del tema para las terjetas

--- session6/fl_componentes/lib/models/theme/app_theme.dart
+++ session6/fl_componentes/lib/models/theme/app_theme.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primary = Colors.green;
+  static const Color primary = Colors.red;
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    primaryColor: const Color.fromARGB(255, 238, 208, 109),
+    primaryColor: const Color.fromARGB(255, 197, 166, 65),
     appBarTheme: const AppBarTheme(
       backgroundColor: Colors.indigo,
       elevation: 0,
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(foregroundColor: primary),
     ),
   );
 }
