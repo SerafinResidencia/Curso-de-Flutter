5ed30d93f2b0925237e591f084f2fbe6eb1aee4f
serafin <serafin.martinez@devitm.com>
2025-10-20 17:33:08 -0600

    81.- Cmabio de color

--- session6/fl_componentes/lib/models/theme/app_theme.dart
+++ session6/fl_componentes/lib/models/theme/app_theme.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primary = Colors.red;
+  static const Color primary = Colors.indigo;
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    primaryColor: const Color.fromARGB(255, 197, 166, 65),
+    primaryColor: const Color.fromARGB(255, 65, 160, 197),
     appBarTheme: const AppBarTheme(
       backgroundColor: Colors.indigo,
       elevation: 0,
     ),
     textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(foregroundColor: primary),
+      style: TextButton.styleFrom(
+        foregroundColor: const Color.fromARGB(255, 120, 63, 181),
+      ),
     ),
   );
 }
