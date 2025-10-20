4247ff945acded54517b3015d71704c8de1a52f5
serafin <serafin.martinez@devitm.com>
2025-10-19 21:58:34 -0600

    78.- Creación de la estructura general de Card Widget

--- session6/fl_componentes/lib/screens/cards_screen.dart
+++ session6/fl_componentes/lib/screens/cards_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_componentes/models/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CardsScreen extends StatelessWidget {
@@ -5,6 +6,32 @@ class CardsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('HomeScreen')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('CardScreen')),
+      body: ListView(
+        padding: const EdgeInsetsGeometry.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
+        children: const [
+          Card(
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(
+                    Icons.phone_bluetooth_speaker,
+                    color: AppTheme.primary,
+                  ),
+                  title: Text('First Title'),
+                  subtitle: Text(
+                    'In sint proident esse anim est aute aliquip mollit laboris officia aliqua cupidatat laborum.',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
