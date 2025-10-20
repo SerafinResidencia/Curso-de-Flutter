e2bc97e89380cf0bb17c8be12b7966288fedd4d1
serafin <serafin.martinez@devitm.com>
2025-10-20 17:33:48 -0600

    81.- Creación del contenedor de imagen

--- session6/fl_componentes/lib/widgets/custom_card_type_2.dart
+++ session6/fl_componentes/lib/widgets/custom_card_type_2.dart
@@ -1,3 +1,4 @@
+import 'package:fl_componentes/models/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
@@ -6,14 +7,28 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 10,
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(20),
+      ),
+      elevation: 20,
+      shadowColor: AppTheme.primary,
       child: Column(
         children: [
-          FadeInImage(
+          const FadeInImage(
             image: NetworkImage(
               'https://d150u0abw3r906.cloudfront.net/wp-content/uploads/2021/10/image15.png',
             ),
             placeholder: AssetImage('assets/jar-loading.gif'),
+            width: double.infinity,
+            height: 230,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 300),
+          ),
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: EdgeInsets.only(right: 20, top: 10, bottom: 10),
+            child: Text('Este es un hermoso paisaje.'),
           ),
         ],
       ),
