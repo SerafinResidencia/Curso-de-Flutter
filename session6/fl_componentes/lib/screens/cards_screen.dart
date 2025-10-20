cf8741a639948840796922f6c0948258a5a073dc
serafin <serafin.martinez@devitm.com>
2025-10-19 22:31:50 -0600

    79.- Creación de la primer tarjeta

--- session6/fl_componentes/lib/screens/cards_screen.dart
+++ session6/fl_componentes/lib/screens/cards_screen.dart
@@ -1,4 +1,4 @@
-import 'package:fl_componentes/models/theme/app_theme.dart';
+import 'package:fl_componentes/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class CardsScreen extends StatelessWidget {
@@ -13,24 +13,7 @@ class CardsScreen extends StatelessWidget {
           horizontal: 20,
           vertical: 10,
         ),
-        children: const [
-          Card(
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(
-                    Icons.phone_bluetooth_speaker,
-                    color: AppTheme.primary,
-                  ),
-                  title: Text('First Title'),
-                  subtitle: Text(
-                    'In sint proident esse anim est aute aliquip mollit laboris officia aliqua cupidatat laborum.',
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        children: const [CustomCardType1()],
       ),
     );
   }
