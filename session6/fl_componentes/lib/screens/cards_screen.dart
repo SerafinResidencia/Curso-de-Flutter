2f27670db4bdfd75e5f50e169d0203697afb1dfe
serafin <serafin.martinez@devitm.com>
2025-10-21 11:14:31 -0600

    86.- modificación de imagen

--- session6/fl_componentes/lib/screens/cards_screen.dart
+++ session6/fl_componentes/lib/screens/cards_screen.dart
@@ -16,18 +16,11 @@ class CardsScreen extends StatelessWidget {
         children: const [
           CustomCardType1(),
           SizedBox(height: 10),
-          CustomCardType2(
-            imageUrl:
-                'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2103',
-          ),
-          CustomCardType2(
-            imageUrl:
-                'https://www.muyinteresante.com/wp-content/uploads/sites/5/2023/09/14/65031bd0e7138.jpeg?resize=1024,683',
-          ),
+          CustomCardType2(imageUrl: 'https://picsum.photos/400/250'),
+          CustomCardType2(imageUrl: 'https://picsum.photos/400/250'),
           CustomCardType2(
             name: "Un hermoso paisaje",
-            imageUrl:
-                'https://121clicks.com/wp-content/uploads/2024/09/best-top-travel-landscape-photography-09.jpg',
+            imageUrl: 'https://picsum.photos/400/250',
           ),
         ],
       ),
