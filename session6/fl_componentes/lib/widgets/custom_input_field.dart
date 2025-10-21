973d2e954bdc00059458c08a0dbf14e824ca579c
serafin <serafin.martinez@devitm.com>
2025-10-21 16:00:23 -0600

    91.- Creación del widget del Form

---
+++ session6/fl_componentes/lib/widgets/custom_input_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CustomInputField extends StatelessWidget {
+  final String? hintText;
+  final String? labelText;
+  final String? helperText;
+  final String? counterText;
+  final IconData? icon;
+  final IconData? sufffixIcon;
+
+  const CustomInputField({
+    super.key,
+    this.hintText,
+    this.labelText,
+    this.helperText,
+    this.icon,
+    this.sufffixIcon,
+    this.counterText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autofocus: true,
+      initialValue: '',
+      textCapitalization: TextCapitalization.words,
+      onChanged: (value) {
+        print('value:$value');
+      },
+      validator: (value) {
+        if (value == null) return 'Este campo es obligatorio';
+        return value.length < 3 ? 'Mínimo de 3 letras' : null;
+      },
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      decoration: InputDecoration(
+        hintText: hintText,
+        labelText: labelText,
+        helperText: helperText,
+        counterText: counterText,
+        icon: icon == null ? null : Icon(Icons.assignment_ind_outlined),
+        suffixIcon: sufffixIcon == null ? null : Icon(Icons.group_add_outlined),
+        /*
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+        ),*/
+      ),
+    );
+  }
+}
