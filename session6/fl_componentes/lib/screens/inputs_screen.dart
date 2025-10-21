8a4f2ab8b9c3c5c93c6e60b518708ea08cefa467
serafin <serafin.martinez@devitm.com>
2025-10-21 16:01:41 -0600

    91.- Creación de la pantalla de inputs

--- session6/fl_componentes/lib/screens/inputs_screen.dart
+++ session6/fl_componentes/lib/screens/inputs_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_componentes/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class InputsScreen extends StatelessWidget {
@@ -12,32 +13,11 @@ class InputsScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
             children: [
-              TextFormField(
-                autofocus: true,
-                initialValue: '',
-                textCapitalization: TextCapitalization.words,
-                onChanged: (value) {
-                  print('value:$value');
-                },
-                validator: (value) {
-                  if (value == null) return 'Este campo es obligatorio';
-                  return value.length < 3 ? 'Mínimo de 3 letras' : null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: InputDecoration(
-                  hintText: 'Nombre del usuaio',
-                  labelText: 'Nombre',
-                  helperText: 'Solo letras',
-                  counterText: '3 carácteres',
-                  icon: Icon(Icons.assignment_ind_outlined),
-                  suffixIcon: Icon(Icons.group_add_outlined),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                    ),
-                  ),
-                ),
+              CustomInputField(
+                labelText: 'Nombre',
+                helperText: 'Solo letras',
+                hintText: 'Nombre del usuario',
+                counterText: 'Almenos 3 letras',
               ),
             ],
           ),
