d182769a80e02ccf27768a410552e73d92ac2310
serafin <serafin.martinez@devitm.com>
2025-10-21 00:00:29 -0600

    84.- Personalizando y mostrando el cuadro de dialogo de la Alerta

--- session6/fl_componentes/lib/screens/alert_screen.dart
+++ session6/fl_componentes/lib/screens/alert_screen.dart
@@ -3,6 +3,36 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({super.key});
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          elevation: 2,
+          title: const Text('Titulo'),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusDirectional.circular(10),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Este es el contenido de la alerta'),
+              SizedBox(height: 10),
+              FlutterLogo(size: 100),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancelar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +47,12 @@ class AlertScreen extends StatelessWidget {
             padding: EdgeInsets.all(20),
             child: Text('Mostrar alerta', style: TextStyle(fontSize: 16)),
           ),
-          onPressed: () {},
+          onPressed: () => displayDialog(context),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.close_sharp),
-        onPressed: () {},
+        onPressed: () => Navigator.pop(context),
       ),
     );
   }
