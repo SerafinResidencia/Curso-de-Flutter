012960e3e1b8f3c7eae51624c28009496239ac22
serafin <serafin.martinez@devitm.com>
2025-10-21 16:01:08 -0600

    91.- Creación de la configuración del tema de Form

--- session6/fl_componentes/lib/models/theme/app_theme.dart
+++ session6/fl_componentes/lib/models/theme/app_theme.dart
@@ -30,5 +30,33 @@ class AppTheme {
         elevation: 0,
       ),
     ),
+
+    //Decoration de Border de Forms
+    inputDecorationTheme: const InputDecorationTheme(
+      floatingLabelStyle: TextStyle(color: primary),
+
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: primary),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
+
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: primary),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
+
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
+    ),
   );
 }
