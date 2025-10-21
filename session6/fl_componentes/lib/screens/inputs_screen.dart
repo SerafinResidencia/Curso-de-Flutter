124ccc7ae4bc9d03924a24d58bf9876f38c851ae
serafin <serafin.martinez@devitm.com>
2025-10-21 16:55:47 -0600

    93.- Widget Form, evalua las conduciones de cada espacio del formulario

--- session6/fl_componentes/lib/screens/inputs_screen.dart
+++ session6/fl_componentes/lib/screens/inputs_screen.dart
@@ -6,43 +6,63 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final myFormKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(title: Text('Inputs y Fomrs')),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(
-            children: [
-              CustomInputField(
-                labelText: 'Nombre',
-                helperText: 'Solo letras',
-                hintText: 'Nombre del usuario',
-                //counterText: 'Almenos 3 letras',
-              ),
-              SizedBox(height: 30),
+          child: Form(
+            key: myFormKey,
+            child: Column(
+              children: [
+                CustomInputField(
+                  labelText: 'Nombre',
+                  helperText: 'Solo letras',
+                  hintText: 'Nombre del usuario',
+                  //counterText: 'Almenos 3 letras',
+                ),
+                SizedBox(height: 30),
 
-              CustomInputField(
-                labelText: 'Apellido',
-                helperText: 'Solo letras',
-                hintText: 'Apellido del usuario',
-              ),
-              SizedBox(height: 30),
+                CustomInputField(
+                  labelText: 'Apellido',
+                  helperText: 'Solo letras',
+                  hintText: 'Apellido del usuario',
+                ),
+                SizedBox(height: 30),
 
-              CustomInputField(
-                labelText: 'Correo',
-                hintText: 'Correo del usuario',
-                keyboardType: TextInputType.emailAddress,
-              ),
-              SizedBox(height: 30),
+                CustomInputField(
+                  labelText: 'Correo',
+                  hintText: 'Correo del usuario',
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(height: 30),
 
-              CustomInputField(
-                labelText: 'Contraseña',
-                hintText: 'Contraseña del usuario',
-                keyboardType: TextInputType.emailAddress,
-                obscureText: true,
-              ),
-              SizedBox(height: 30),
-            ],
+                CustomInputField(
+                  labelText: 'Contraseña',
+                  hintText: 'Contraseña del usuario',
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: true,
+                ),
+                SizedBox(height: 30),
+
+                ElevatedButton(
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: Center(child: Text('Guardar')),
+                  ),
+                  onPressed: () {
+                    FocusScope.of(context).requestFocus(
+                      FocusNode(),
+                    ); //Permite ocultar el teclado al pulsar el botón.
+                    if (!myFormKey.currentState!.validate()) {
+                      print('Formulario no válido');
+                      return;
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
