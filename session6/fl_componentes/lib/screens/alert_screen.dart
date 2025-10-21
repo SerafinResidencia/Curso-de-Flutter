0eb27c89c47673ab745b43117bedf528b55c31a3
serafin <serafin.martinez@devitm.com>
2025-10-21 00:17:52 -0600

    85.- Cupertino Dialog para IOS

--- session6/fl_componentes/lib/screens/alert_screen.dart
+++ session6/fl_componentes/lib/screens/alert_screen.dart
@@ -1,9 +1,45 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({super.key});
 
-  void displayDialog(BuildContext context) {
+  void displayDialogIOS(BuildContext context) {
+    showCupertinoDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Este es el contenido de la alerta'),
+              SizedBox(height: 10),
+              FlutterLogo(size: 100),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text(
+                'Cancelar',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Ok'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void displayDialogAndroid(BuildContext context) {
     showDialog(
       barrierDismissible: false,
       context: context,
@@ -27,6 +63,10 @@ class AlertScreen extends StatelessWidget {
               onPressed: () => Navigator.pop(context),
               child: const Text('Cancelar'),
             ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Ok'),
+            ),
           ],
         );
       },
@@ -47,7 +87,11 @@ class AlertScreen extends StatelessWidget {
             padding: EdgeInsets.all(20),
             child: Text('Mostrar alerta', style: TextStyle(fontSize: 16)),
           ),
-          onPressed: () => displayDialog(context),
+          //onPressed: () => displayDialogAndroid(context),
+          //onPressed: () => displayDialogIOS(context),
+          onPressed: () => Platform.isAndroid
+              ? displayDialogAndroid(context)
+              : displayDialogIOS(context),
         ),
       ),
       floatingActionButton: FloatingActionButton(
