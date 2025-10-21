47b320d0d244ed114740707b323b181b1e478535
serafin <serafin.martinez@devitm.com>
2025-10-21 14:29:14 -0600

    90.- TextFomrFields - Personalización

--- session6/fl_componentes/lib/screens/inputs_screen.dart
+++ session6/fl_componentes/lib/screens/inputs_screen.dart
@@ -19,6 +19,25 @@ class InputsScreen extends StatelessWidget {
                 onChanged: (value) {
                   print('value:$value');
                 },
+                validator: (value) {
+                  if (value == null) return 'Este campo es obligatorio';
+                  return value.length < 3 ? 'Mínimo de 3 letras' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  hintText: 'Nombre del usuaio',
+                  labelText: 'Nombre',
+                  helperText: 'Solo letras',
+                  counterText: '3 carácteres',
+                  icon: Icon(Icons.assignment_ind_outlined),
+                  suffixIcon: Icon(Icons.group_add_outlined),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
