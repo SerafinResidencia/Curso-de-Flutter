a808bed65761199248839e369159b8f3a0a95076
serafin <serafin.martinez@devitm.com>
2025-10-21 14:09:48 -0600

    89.- Exportamos Inputs Screen

--- session6/fl_componentes/lib/screens/screens.dart
+++ session6/fl_componentes/lib/screens/screens.dart
@@ -1,7 +1,8 @@
+export 'package:fl_componentes/screens/alert_screen.dart';
 export 'package:fl_componentes/screens/animated_screen.dart';
 export 'package:fl_componentes/screens/avatar_screen.dart';
 export 'package:fl_componentes/screens/cards_screen.dart';
-export 'package:fl_componentes/screens/alert_screen.dart';
 export 'package:fl_componentes/screens/home_screen.dart';
+export 'package:fl_componentes/screens/inputs_screen.dart';
 export 'package:fl_componentes/screens/listview1_screen.dart';
 export 'package:fl_componentes/screens/listview2_screen.dart';
