edf117b93f296b9283370d68d098fc006c34eb34
serafin <serafin.martinez@devitm.com>
2025-10-21 14:08:07 -0600

    89.- Creació de Pantalla de Forms

---
+++ session6/fl_componentes/lib/screens/inputs_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class InputsScreen extends StatelessWidget {
+  const InputsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Inputs y Fomrs')),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            children: [
+              TextFormField(
+                autofocus: true,
+                initialValue: '',
+                textCapitalization: TextCapitalization.words,
+                onChanged: (value) {
+                  print('value:$value');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
