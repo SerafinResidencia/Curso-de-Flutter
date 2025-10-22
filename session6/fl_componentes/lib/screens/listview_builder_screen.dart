3f9c26d188f14b6262356f917e174216fc7ce59b
serafin <serafin.martinez@devitm.com>
2025-10-21 23:03:13 -0600

    98.- Inifite Scroll

--- session6/fl_componentes/lib/screens/listview_builder_screen.dart
+++ session6/fl_componentes/lib/screens/listview_builder_screen.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 
-class ListviewBuilderScreen extends StatelessWidget {
+class ListviewBuilderScreen extends StatefulWidget {
   const ListviewBuilderScreen({super.key});
+
+  @override
+  State<ListviewBuilderScreen> createState() => _ListviewBuilderScreenState();
+}
+
+class _ListviewBuilderScreenState extends State<ListviewBuilderScreen> {
+  final List<int> imagenesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        add5();
+      }
+    });
+  }
+
+  void add5() {
+    final lastId = imagenesIds.last;
+    imagenesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +36,9 @@ class ListviewBuilderScreen extends StatelessWidget {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
-          itemCount: 10,
+          physics: const BouncingScrollPhysics(),
+          controller: scrollController,
+          itemCount: imagenesIds.length,
           itemBuilder: (BuildContext context, int index) {
             return FadeInImage(
               width: double.infinity,
@@ -19,7 +46,7 @@ class ListviewBuilderScreen extends StatelessWidget {
               fit: BoxFit.cover,
               placeholder: const AssetImage('assets/jar-loading.gif'),
               image: NetworkImage(
-                'https://picsum.photos/500/300?random=$index',
+                'https://picsum.photos/500/300?random=${imagenesIds[index]}',
               ),
             );
           },
