71fa9f36d39033b730eceeb6198023dbefabc519
serafin <serafin.martinez@devitm.com>
2025-10-22 11:15:11 -0600

    115. Poster y título de la película

--- session7/peliculas/lib/screens/details_screen.dart
+++ session7/peliculas/lib/screens/details_screen.dart
@@ -5,15 +5,30 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //TODO: Cambiar por una instacia de movie
+    // TODO: Cambiar por una instancia de movie
     final String movie =
         ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
 
-    return Scaffold(body: CustomScrollView(slivers: [_CustomAppBar()]));
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              const _PosterAndTitle(),
+              const SizedBox(height: 50),
+              Center(child: Text('Hola $movie')),
+            ]),
+          ),
+        ],
+      ),
+    );
   }
 }
 
 class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar();
+
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
@@ -23,18 +38,85 @@ class _CustomAppBar extends StatelessWidget {
       pinned: true,
       flexibleSpace: FlexibleSpaceBar(
         centerTitle: true,
-        titlePadding: EdgeInsets.all(0),
+        titlePadding: EdgeInsets.zero,
         title: Container(
           width: double.infinity,
           alignment: Alignment.bottomCenter,
-          color: Colors.black12,
-          child: Text('movie.title', style: TextStyle(fontSize: 16)),
+          color: const Color.fromARGB(150, 0, 0, 0),
+          padding: const EdgeInsets.only(bottom: 10),
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16, color: Colors.white),
+          ),
         ),
-        background: FadeInImage(
+        background: const FadeInImage(
           placeholder: AssetImage('assets/loading.gif'),
-          image: NetworkImage('https://picsum.photos/300/400'),
+          image: NetworkImage('https://picsum.photos/500/800'),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle();
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textThem = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Imagen del póster
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://picsum.photos/300/400'),
+              height: 150,
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          const SizedBox(width: 20),
+
+          // Títulos y rating
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'movie.title',
+                  style: textThem.headlineMedium,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+                Text(
+                  'movie.originalTitle',
+                  style: textThem.titleMedium,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.star_border_purple500,
+                      size: 15,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(width: 5),
+                    Text('movie.voteAverage', style: textThem.bodySmall),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
