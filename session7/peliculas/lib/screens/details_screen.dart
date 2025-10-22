69d339a21bb09104ff8ba32b8d0fe20c83f94078
serafin <serafin.martinez@devitm.com>
2025-10-22 10:40:28 -0600

    114.- MovieDetails - Slivers - SliverAppBar

--- session7/peliculas/lib/screens/details_screen.dart
+++ session7/peliculas/lib/screens/details_screen.dart
@@ -5,6 +5,37 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Details Screen')));
+    //TODO: Cambiar por una instacia de movie
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
+    return Scaffold(body: CustomScrollView(slivers: [_CustomAppBar()]));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text('movie.title', style: TextStyle(fontSize: 16)),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://picsum.photos/300/400'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }
