405147d6c1b31f65edb654aaa902e9c64f0e7b3b
serafin <serafin.martinez@devitm.com>
2025-10-24 17:28:46 -0600

    132.- Mostrar las películas basado en el query

--- session7/peliculas/lib/search/search_delegate.dart
+++ session7/peliculas/lib/search/search_delegate.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/models/movie.dart';
+import 'package:peliculas/providers/movies_provider.dart';
+import 'package:provider/provider.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
   @override
-  String get searchFieldLabel => 'Buscar...';
+  String get searchFieldLabel => 'Buscar pelicula o serie';
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [IconButton(icon: Icon(Icons.clear), onPressed: () => query = '')];
@@ -23,11 +26,57 @@ class MovieSearchDelegate extends SearchDelegate {
     return Text('buildResults');
   }
 
+  Widget _empyContainer() {
+    return Container(
+      child: Center(
+        child: Icon(
+          Icons.movie_filter_rounded,
+          color: Colors.black38,
+          size: 130,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget buildSuggestions(BuildContext context) {
     if (query.isEmpty) {
-      return Container(child: Center(child: Icon(Icons.movie_filter_rounded)));
+      return _empyContainer();
     }
-    return Container();
+
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+    return FutureBuilder(
+      future: moviesProvider.searchMovies(query),
+      builder: (_, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) return _empyContainer();
+        final movies = snapshot.data;
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (_, index) => movies[index],
+        );
+      },
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem({super.key, required this.movie});
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: FadeInImage(
+        placeholder: AssetImage('assets/no-image.jpg'),
+        image: NetworkImage(movie.fullPosterImg),
+        width: 50,
+        fit: BoxFit.contain,
+      ),
+      title: Text(movie.title),
+      subtitle: Text(movie.originalTitle),
+      onTap: () {
+        Navigator.pushNamed(context, 'details', arguments: movie);
+      },
+    );
   }
 }
