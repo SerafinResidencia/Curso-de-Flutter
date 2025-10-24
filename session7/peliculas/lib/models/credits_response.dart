aa2a56a280edcf2c414502779edee5bb703cf387
serafin <serafin.martinez@devitm.com>
2025-10-23 22:55:13 -0600

    127.- Lista de actores de la película

---
+++ session7/peliculas/lib/models/credits_response.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+class PopularResponse {
+  int id;
+  List<Cast> cast;
+  List<Cast> crew;
+
+  PopularResponse({required this.id, required this.cast, required this.crew});
+
+  factory PopularResponse.fromJson(String str) =>
+      PopularResponse.fromMap(json.decode(str));
+
+  factory PopularResponse.fromMap(Map<String, dynamic> json) => PopularResponse(
+    id: json["id"],
+    cast: List<Cast>.from(json["cast"].map((x) => Cast.fromMap(x))),
+    crew: List<Cast>.from(json["crew"].map((x) => Cast.fromMap(x))),
+  );
+}
+
+class Cast {
+  bool adult;
+  int gender;
+  int id;
+  String knownForDepartment;
+  String name;
+  String originalName;
+  double popularity;
+  String? profilePath;
+  int? castId;
+  String? character;
+  String creditId;
+  int? order;
+  String? department;
+  String? job;
+
+  Cast({
+    required this.adult,
+    required this.gender,
+    required this.id,
+    required this.knownForDepartment,
+    required this.name,
+    required this.originalName,
+    required this.popularity,
+    required this.profilePath,
+    this.castId,
+    this.character,
+    required this.creditId,
+    this.order,
+    this.department,
+    this.job,
+  });
+  get fullProfilePath {
+    if (profilePath != null) {
+      return 'https://image.tmdb.org/t/p/w500${this.profilePath}';
+    }
+    return 'http://i.stack.imgur.com/GNhxO.jpg';
+  }
+
+  factory Cast.fromJson(String str) => Cast.fromMap(json.decode(str));
+
+  factory Cast.fromMap(Map<String, dynamic> json) => Cast(
+    adult: json["adult"],
+    gender: json["gender"],
+    id: json["id"],
+    knownForDepartment: json["known_for_department"],
+    name: json["name"],
+    originalName: json["original_name"],
+    popularity: json["popularity"]?.toDouble(),
+    profilePath: json["profile_path"],
+    castId: json["cast_id"],
+    character: json["character"],
+    creditId: json["credit_id"],
+    order: json["order"],
+    department: json["department"],
+    job: json["job"],
+  );
+}
--- session7/peliculas/lib/widgets/casting_cards.dart
+++ session7/peliculas/lib/widgets/casting_cards.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CastingCards extends StatelessWidget {
-  const CastingCards({super.key});
-
+  const CastingCards({super.key, required this.movieId});
+  final int movieId;
   @override
   Widget build(BuildContext context) {
     return Container(
