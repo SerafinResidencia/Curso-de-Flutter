1d076d89ce374c9efe88e2058915e8201f908d09
serafin <serafin.martinez@devitm.com>
2025-10-26 20:00:35 -0600

    209.- Contenedor blanco

--- session12/productos_app/lib/screens/login_screen.dart
+++ session12/productos_app/lib/screens/login_screen.dart
@@ -8,10 +8,25 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: AuthBackground(
-        child: Container(
-          width: double.infinity,
-          height: 300,
-          color: Colors.red,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 250),
+              CardContainer(
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    Text(
+                      'Login',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    SizedBox(height: 30),
+                    Text('Formulario'),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
