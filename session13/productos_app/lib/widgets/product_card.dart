f4065ff027a788e26e2b730ea61af5575d223562
serafin <serafin.martinez@devitm.com>
2025-10-27 13:05:24 -0600

    221.- Widget - ProductCard

--- session13/productos_app/lib/widgets/product_card.dart
+++ session13/productos_app/lib/widgets/product_card.dart
@@ -11,7 +11,50 @@ class ProductCard extends StatelessWidget {
         margin: EdgeInsets.only(top: 30, bottom: 50),
         width: double.infinity,
         height: 400,
-        color: Colors.red,
+        decoration: _cardBorders(),
+        child: Stack(
+          alignment: AlignmentGeometry.bottomLeft,
+          children: [_BackgroundImage(), _ProductDetails()],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _cardBorders() {
+    return BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(25),
+      boxShadow: [
+        BoxShadow(color: Colors.black12, offset: Offset(0, 7), blurRadius: 10),
+      ],
+    );
+  }
+}
+
+class _ProductDetails extends StatelessWidget {
+  const _ProductDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: double.infinity, height: 70, color: Colors.red);
+  }
+}
+
+class _BackgroundImage extends StatelessWidget {
+  const _BackgroundImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadiusGeometry.circular(25),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+        ),
       ),
     );
   }
