e5a265006e19abfd8f1475abfa872737f725aac6
serafin <serafin.martinez@devitm.com>
2025-10-27 15:34:19 -0600

    226.- Modelo y Servicio de Productos

---
+++ session13/productos_app/lib/services/products_service.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/models/models.dart';
+
+class ProductsService extends ChangeNotifier {
+  final String _baseUrl =
+      'finalflutter-varios-296cd-default-rtdb.firebaseio.com';
+  final List<Product> products = [];
+
+  //Hacer fetch de los productos
+}
