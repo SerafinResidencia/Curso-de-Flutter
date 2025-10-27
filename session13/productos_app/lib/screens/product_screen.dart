066e7340db78cf63be409ef9b4a83a8ddedb88c0
serafin <serafin.martinez@devitm.com>
2025-10-27 14:41:10 -0600

    224.- Pantalla de producto - Parte 2

--- session13/productos_app/lib/screens/product_screen.dart
+++ session13/productos_app/lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -10,10 +11,102 @@ class ProductScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Stack(children: [ProductImage()]),
+            Stack(
+              children: [
+                ProductImage(),
+
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: Icon(
+                      Icons.arrow_back_ios,
+                      size: 40,
+                      color: (Colors.white),
+                    ),
+                  ),
+                ),
+
+                Positioned(
+                  top: 60,
+                  right: 20,
+                  child: IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: Icon(
+                      Icons.camera_alt_outlined,
+                      size: 40,
+                      color: (Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            _ProductForm(),
+            SizedBox(height: 100),
           ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save_as_rounded),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class _ProductForm extends StatelessWidget {
+  const _ProductForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 10),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+
+        decoration: _buildBoxDecoration(),
+        child: Form(
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: 'Nombre del Producto',
+                  labelText: 'Nombre',
+                ),
+              ),
+              SizedBox(height: 30),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: '\$150',
+                  labelText: 'precio',
+                ),
+              ),
+              SizedBox(height: 30),
+              SwitchListTile.adaptive(
+                value: true,
+                title: Text('Disponible'),
+                activeThumbColor: Colors.indigo,
+                onChanged: (value) {},
+              ),
+              SizedBox(height: 30),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.only(
+      bottomLeft: Radius.circular(25),
+      bottomRight: Radius.circular(25),
+    ),
+    boxShadow: [BoxShadow(color: Colors.black87, offset: Offset(0, 5))],
+  );
 }
